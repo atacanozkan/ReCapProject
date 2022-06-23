a289d99beb8ef457eb8d34fc99228666b69ea71a
Atacan Ozkan <atacanozkan@gmail.com>
2022-06-24 01:01:52 +0300

    Missing files added

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DataAccess.Abstract
+{
+    // Generic constraint
+    // T tipini sınırlandırabilmek için kullanılır
+    // class -> T'nin referans tip alabileceği ve IEntity veya IEntity'yi implement eden bir nesne olabileceği anlamına gelir
+    // new() : new'lenebilir olmalı. IEntity new'lenemeyeceği için sadece IEntity'yi implement eden bir nesne olabilir
+    public interface IEntityRepository<T> where T:class,IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/CarsContext.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : Db tabloları ile proje classlarını bağlamak
+    public class CarsContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ReCap;Trusted_Connection=true");
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Color { get; set; }
+    }
+}
